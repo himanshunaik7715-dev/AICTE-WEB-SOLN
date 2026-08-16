d09aa2b65175c9b9beb7229902e0ead8a6385b14
Himanshu <himanshunaik7715@gmail.com>
2026-08-16 21:36:00 +0530

    hogaya1

--- server.ts
+++ server.ts
@@ -532,8 +532,8 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, 'localhost', () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
   });
 }
 
